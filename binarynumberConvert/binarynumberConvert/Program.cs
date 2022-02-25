5bc0af6912d9ea55f4a8224bf63cc2d9587a775f
burakugras <99265170+burakugras@users.noreply.github.com>
2022-02-25 19:14:09 +0300

    Add files via upload
    In this program, we find the maximum number of 1s in the binary equivalent of an integer entered.

---
+++ binarynumberConvert/binarynumberConvert/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binarynumberConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("input a number: ");
+            //int number = int.Parse(Console.ReadLine());
+            //int max = 0;
+            //int count = 0;
+            //int reminder = 0;
+
+            //while(number>0)
+            //{
+            //    reminder = number % 2;
+            //    number /= 2;
+            //    if (reminder==1)
+            //    {
+            //        count++;
+            //        max = Math.Max(max, count);
+            //    }
+            //    else
+            //    {
+            //        count = 0;
+            //    }
+            //}
+
+            //Console.WriteLine(max);
+
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            int reminder = 0;
+
+            
+            reminder = number % 2;
+            number /= 2;
+            
+            Console.WriteLine(number);
+            Console.ReadLine();
+        }
+    }
+}
